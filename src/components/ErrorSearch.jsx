35410c56dd0885fd52871a069ca1ee2229f87720
GERSON <gersonllamas@hotmail.com>
2024-02-28 23:34:18 -0600

    commit texto nuevo

--- src/components/ErrorSearch.jsx
+++ src/components/ErrorSearch.jsx
@@ -2,7 +2,7 @@ const ErrorSearch = () => {
   return (
 
     <div>
-           <h1> 😢 Este valor no es un pais valido, ingresa otro, por favor</h1>
+           <h1> 😢 Este valor no es admitido por Berenice aqui puras cosas fancys</h1>
            <img className="imgErr" src="https://http.cat/images/102.jpg" alt="gatito 404" />
     </div>
 
